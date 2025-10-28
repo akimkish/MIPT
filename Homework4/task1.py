3e7f0bd02d77a83f7fa32bc6b9d3162baa8b17c8
akimkish <kish.eik14@gmail.com>
2025-10-28 22:29:08 +0300

    completed

--- Homework4/task1.py
+++ Homework4/task1.py
@@ -31,7 +31,7 @@ class Library(BaseModel):
 
 
 class BookNotAvailable(Exception):
-    def __init__(self, msg="This book is not available!"):
+    def __init__(self, msg="This book is not available!") -> NoReturn:
         self.message = msg
         super().__init__(self.message)
 
@@ -49,11 +49,20 @@ def find_book(library: Library, book: Book) -> str:
         return f"The book {book.author}-{book.title} is not on the Library's list."
 
 
-def is_book_borrow(book: Book) -> str:
-    if book.available == True:
-        return f"The book {book.author}-{book.title} is available now!"
-    else:
-        return f"The book {book.author}-{book.title} is borrowed!"
+def is_book_borrow(book: Book) -> NoReturn:
+    # Task 2
+    #  if book.available == True:
+    #     return f"The book {book.author}-{book.title} is available now!"
+    # else:
+    #     return f"The book {book.author}-{book.title} is borrowed!"
+    # Task 4
+    try:
+        if book.available == False:
+            raise BookNotAvailable
+        else:
+            print(f"The book {book.author}-{book.title} is available now!")
+    except BookNotAvailable as exp:
+        print(f"{book.author}-{book.title}. {exp}")
 
 
 def return_book(book: Book) -> NoReturn:
@@ -66,37 +75,40 @@ def return_book(book: Book) -> NoReturn:
         )
 
 
-Demian = Book(
-    title="Demian",
-    author="Herman Hesse",
-    year=1919,
-    available=True,
-    categories=["World classic"],
-)
-Sunday = Book(
-    title="Voskresenie",
-    author="Lev Tolstoy",
-    year=1899,
-    available=True,
-    categories=["Russian classic"],
-)
-Fairytale = Book(
-    title="FairyTale",
-    author="Aleksander Pushkin",
-    year=1830,
-    available=True,
-    categories=["Russian classic"],
-)
-
+# Test
 if __name__ == "__main__":
+    Demian = Book(
+        title="Demian",
+        author="Herman Hesse",
+        year=1919,
+        available=True,
+        categories=["World classic"],
+    )
+    Sunday = Book(
+        title="Voskresenie",
+        author="Lev Tolstoy",
+        year=1899,
+        available=True,
+        categories=["Russian classic"],
+    )
+    FairyTales = Book(
+        title="Fairy Tales",
+        author="Aleksander Pushkin",
+        year=1830,
+        available=True,
+        categories=["Russian fairy tales"],
+    )
     My_labrirary = Library(books=[], users=[])
     add_book(My_labrirary, Demian)
     add_book(My_labrirary, Sunday)
-    add_book(My_labrirary, Fairytale)
+    add_book(My_labrirary, FairyTales)
     print(My_labrirary.books)
     My_labrirary.total_books()
     return_book(Demian)
     Sunday.available = False
+    is_book_borrow(Sunday)
     My_labrirary.total_books()
     return_book(Sunday)
     My_labrirary.total_books()
+    is_book_borrow(Sunday)
+    print(find_book(My_labrirary, FairyTales))
